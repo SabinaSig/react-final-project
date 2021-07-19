1c73cd62ac079062b6dd1568d831459d1f383e5b
SabinaSig <77678314+SabinaSig@users.noreply.github.com>
2021-07-19 23:15:36 +0200

    Creating an app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+
+export default function App () {
+  return (
+    <div>
+    <h1>React projects portfolio</h1>
+    <h2>What would you like to see?</h2>
+    <div className="row">
+    <div className="col-3">
+    <img src="" alt="" />
+    </div>
+    <div className="col-3">
+    <img src="" alt="" />
+    </div>
+    </div>
+    </div>
+  );
+}
